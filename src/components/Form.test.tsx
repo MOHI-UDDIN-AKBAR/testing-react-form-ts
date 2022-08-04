f92fd5b9379c5ce1ca5a86619d620c29b5409491
Mohi Uddin Akbar <makbar@WHM0005366.local>
2022-08-04 23:29:26 +0200

    modified

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,13 +1,39 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render, cleanup, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import user from "@testing-library/user-event";
 import Form from "./Form";
+interface FormTypes {
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
+afterEach(cleanup);
+const typeIntoForm = ({ email, password, confirmPassword }: FormTypes) => {
+  const emailElement: HTMLElement = screen.getByLabelText(/email:/i);
+  const passwordElement: HTMLElement = screen.getByLabelText("Password:");
+  const confirmPasswordElement: HTMLElement =
+    screen.getByLabelText(/confirm-password/i);
 
+  if (email) {
+    user.type(emailElement, email);
+  }
+  if (password) {
+    user.type(passwordElement, password);
+  }
+  if (confirmPassword) {
+    user.type(confirmPasswordElement, confirmPassword);
+  }
+  return {
+    emailElement,
+    passwordElement,
+    confirmPasswordElement,
+  };
+};
+beforeEach(() => {
+  render(<Form />);
+});
 describe("Initial value for every input is empty string", () => {
-  beforeEach(() => {
-    render((<Form />) as JSX.Element);
-  });
-
   test("email value should be empty string", () => {
     const emailElement = screen.getByLabelText(/email:/i);
     expect((emailElement as HTMLInputElement).value).toBe("");
@@ -23,60 +49,48 @@ describe("Initial value for every input is empty string", () => {
 });
 
 describe("after typing expected value", () => {
-  beforeEach(() => {
-    render((<Form />) as JSX.Element);
-  });
+  //   beforeEach(() => {
+  //     render(<Form />);
+  //   });
   test("expected value for email", () => {
-    const emailElement = screen.getByLabelText(/email:/i);
-    user.type(emailElement, "ok@gmail.com");
+    const { emailElement } = typeIntoForm({ email: "ok@gmail.com" });
     expect((emailElement as HTMLInputElement).value).toBe("ok@gmail.com");
   });
   test("password value for email", () => {
-    const passwordElement = screen.getByLabelText("Password:");
-    user.type(passwordElement, "plm09@#");
+    const { passwordElement } = typeIntoForm({ password: "plm09@#" });
     expect((passwordElement as HTMLInputElement).value).toBe("plm09@#");
   });
   test("confirm password value for email", () => {
-    const confirmPasswordElement = screen.getByLabelText(/confirm-Password/i);
-    user.type(confirmPasswordElement, "123plm");
+    const { confirmPasswordElement } = typeIntoForm({
+      confirmPassword: "123plm",
+    });
     expect((confirmPasswordElement as HTMLInputElement).value).toBe("123plm");
   });
 });
 
 describe("ger Error if input is empty after submit", () => {
-  beforeEach(() => {
-    render((<Form />) as JSX.Element);
-  });
+  //   beforeEach(() => {
+  //     render(<Form />);
+  //   });
   it("get error if email is invalid", () => {
-    const emailElement = screen.getByLabelText(/email:/i);
     const buttonElement = screen.getByRole("button");
     const errorText = screen.queryByText(/in valid email/i);
     expect(errorText as HTMLElement).not.toBeInTheDocument();
-    user.type(emailElement, "aohofhscom");
+    const { emailElement } = typeIntoForm({ email: "aohofhscom" });
     user.click(buttonElement);
     const errorTextAgain = screen.queryByText(/in valid email/i);
     expect(errorTextAgain as HTMLElement).toBeInTheDocument();
   });
-  it("get error if email is empty", () => {
-    const emailElement = screen.getByLabelText(/email:/i);
-    const buttonElement = screen.getByRole("button");
-    const errorText = screen.queryByText(/email is empty/i);
-    expect(errorText as HTMLElement).not.toBeInTheDocument();
-    user.type(emailElement, "");
-    user.click(buttonElement);
-    const errorTextAgain = screen.queryByText(/email is empty/i);
-    expect(errorTextAgain as HTMLElement).toBeInTheDocument();
-  });
+
   it("get error if password is empty", () => {
-    const emailElement = screen.getByLabelText(/email:/i);
-    user.type(emailElement, "");
-    const passwordElement = screen.getByLabelText("Password:");
+    const { emailElement } = typeIntoForm({ email: "aohofhscom" });
     const errorElement = screen.queryByText(
       /in valid password. should be at least 5 character/i
     );
     const buttonElement = screen.getByRole("button");
     expect(errorElement).not.toBeInTheDocument();
-    user.type(passwordElement, "");
+    const { passwordElement } = typeIntoForm({ password: "1234" });
+
     user.click(buttonElement);
     const errorElementAgain = screen.queryByText(
       /in valid password. should be at least 5 character/i
@@ -84,23 +98,28 @@ describe("ger Error if input is empty after submit", () => {
     expect(errorElementAgain).toBeInTheDocument();
   });
   it("if confirm password does not match with upper password", () => {
-    const emailElement = screen.getByLabelText(/email:/i);
-    const passwordElement = screen.getByLabelText("Password:");
-    const confirmPasswordElement = screen.getByLabelText(/confirm-password/i);
-    user.type(emailElement, "");
+    const { emailElement } = typeIntoForm({ email: "mma@arafat" });
+
     const buttonElement = screen.getByRole("button");
     const errorElement = screen.queryByText(/in valid Matching/i);
     expect(errorElement).not.toBeInTheDocument();
-    user.type(passwordElement, "hello");
-    user.type(confirmPasswordElement, "helllo");
+
+    const { passwordElement } = typeIntoForm({ password: "hello" });
+    const { confirmPasswordElement } = typeIntoForm({
+      confirmPassword: "helllo",
+    });
+
     user.click(buttonElement);
     const errorElementAgain = screen.queryByText(/in valid Matching/i);
     expect(errorElementAgain).toBeInTheDocument();
   });
   it("if everything is ok then no error message", () => {
-    const emailElement = screen.getByLabelText(/email:/i);
-    const passwordElement = screen.getByLabelText("Password:");
-    const confirmPasswordElement = screen.getByLabelText(/confirm-password/i);
+    const { emailElement } = typeIntoForm({ email: "mma@arafat" });
+    const { passwordElement } = typeIntoForm({ password: "123456" });
+    const { confirmPasswordElement } = typeIntoForm({
+      confirmPassword: "123456",
+    });
+
     const errorElementOne = screen.queryByText(/in valid email/i);
     const errorElementTwo = screen.queryByText(
       /in valid password. should be at least 5 character/i
